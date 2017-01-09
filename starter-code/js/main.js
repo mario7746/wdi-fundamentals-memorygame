f04782e134761d20f76ad1b0609db83c53849cd7
mario7746 <m.kennedykavouras@gmail.com>
2017-01-08 19:27:19 -0500

    rendered cards with JS

--- starter-code/js/main.js
+++ starter-code/js/main.js
@@ -1,12 +1,33 @@
 console.log("JS file is connected to HTML! Woo!")
 
-var cardOne = 'queen';
-var cardTwo = 'queen';
-var cardThree = 'king';
-var cardFour = 'king';
+var board = document.getElementById('board-game')
 
-if (cardOne === cardTwo) {
-	alert('You found a match!');	
-} else {
-  alert('Sorry, try again.');
+function createCards() {
+	var cardElement;
+
+	for (var i = 0 ; i<=3; i++) {
+		cardElement = document.createElement('div')
+		cardElement.className = 'card'
+
+		board.appendChild(cardElement)
+	}
 }
+createCards()
+
+
+// var cardOne = 'queen';
+// var cardTwo = 'queen';
+// var cardThree = 'king';
+// var cardFour = 'king';
+
+// if (cardOne === cardFour) {
+//   alert = "Sorry try again";
+// } else if (cardTwo === cardThree) {
+//   alert  ("Sorry try again");
+// } else if (cardOne === cardTwo) {
+//   alert ("You've found a match!");
+// } else if (cardThree === cardFour) {
+//   alert ("You've found a match!");	
+// } else {
+//   alert ("Nothing");
+// }
